31b5509ecd19cbb767b9943aace06b1deb05900e
Tom Radford <tom@theradford.com>
2024-04-29 22:48:54 +0200

    add BackIcon component

---
+++ src/components/common/icons/BackIcon.tsx
@@ -0,0 +1,28 @@
+const BackIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9.57 5.93L3.5 12L9.57 18.07"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M20.5 12H3.67004"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default BackIcon;
