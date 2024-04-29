bf8fcc75d412f516a6df0b259f8cfe8705598df5
Tom Radford <tom@theradford.com>
2024-04-29 18:23:34 +0200

    add shared application colours

---
+++ src/utils/constants.ts
@@ -0,0 +1,26 @@
+/**
+ * @constant
+ * Used to for the office colour selector.
+ *
+ * Opted over tailwind config colours to be able to
+ * render colour list selector from this
+ * array and to easily set in the DB
+ */
+export const OFFICE_COLOURS = [
+  "#FFBE0B",
+  "#FF9B71",
+  "#FB5607",
+  "#97512C",
+  "#DBBADD",
+  "#FF006E",
+  "#A9F0D1",
+  "#00B402",
+  "#489DDA",
+  "#0072E8",
+  "8338EC",
+];
+/**
+ * @constant
+ * Title for the app
+ */
+export const TITLE = "Office Space Manager";
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,19 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
-
+import colors from "tailwindcss/colors";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    colors: {
+      background: "#F8FAFC",
+      white: colors.white,
+      black: colors.black,
+      primary: "#0D4477",
+      secondary: "#489DDA",
+      gray: "#484954",
+      // Office colours are defined in the `OFFICE_COLOURS` constant
+    },
+    // Keeping tailwind defaults (sans colours) for convenience
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
