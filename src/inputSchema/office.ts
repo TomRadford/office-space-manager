4d61976d4a38d14a5a9916cd8b99ee0c265d8c13
Tom Radford <tom@theradford.com>
2024-04-29 23:06:21 +0200

    create an office schema for input

---
+++ src/inputSchema/office.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+/**
+ * Schema for office input validation
+ */
+export const officeInputSchema = z.object({
+  name: z.string().min(3),
+  address: z.string(),
+  email: z.string().email(),
+  phone: z.string().min(10),
+  maximumCapacity: z.number(),
+});
