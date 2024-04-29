e39e87d6ed0492545d44b4904279cfb2c4b369f6
Tom Radford <tom@theradford.com>
2024-04-29 22:50:29 +0200

    add simple text input

---
+++ src/components/common/input/TextInput.tsx
@@ -0,0 +1,10 @@
+const TextInput = () => {
+  return (
+    <input
+      type="text"
+      className="w-full border-0 placeholder:opacity-50 focus:ring-secondary"
+      placeholder="Office Name"
+    />
+  );
+};
+export default TextInput;
