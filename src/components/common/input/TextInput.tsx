b002e6fc547f1bdba14cdee6efc6014982b6fff4
Tom Radford <tom@theradford.com>
2024-04-30 09:37:41 +0200

    use ErrorMessage in TextInput

--- src/components/common/input/TextInput.tsx
+++ src/components/common/input/TextInput.tsx
@@ -1,5 +1,7 @@
 import { forwardRef, type InputHTMLAttributes } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
+import { cn } from "@/utils/classname";
+import ErrorMessage from "@/components/common/input/ErrorMessage";
 
 // We pass the ref to the input element,
 // this is necessary for react-hook-form
@@ -8,28 +10,22 @@ const TextInput = forwardRef<
   HTMLInputElement,
   InputHTMLAttributes<HTMLInputElement> & { errorMessage?: string }
 >((props, ref) => {
+  const elementProps = { ...props };
+  delete elementProps.errorMessage;
   return (
     <div className="relative">
       <input
-        className="invalid:ring-red-500 w-full border-0 placeholder:opacity-50 invalid:ring-1 focus:ring-secondary"
-        {...props}
+        className={cn(
+          "w-full border-0 placeholder:opacity-50 focus:ring-secondary",
+          { "ring-1 ring-red focus:ring-red": props.errorMessage },
+        )}
+        {...elementProps}
         ref={ref}
       />
       <AnimatePresence>
         {props.errorMessage && (
-          // TODO get correct validation feedback
-          // from the design team, this one is a placeholder
-          // so that the user can see the error message so long
           <div className="absolute -bottom-4 left-3">
-            <motion.p
-              initial={{ opacity: 0, y: -10 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -10 }}
-              transition={{ type: "tween" }}
-              className=" text-xs text-red"
-            >
-              {props.errorMessage}
-            </motion.p>
+            <ErrorMessage errorMessage={props.errorMessage} />
           </div>
         )}
       </AnimatePresence>
