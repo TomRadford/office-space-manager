d9dfdac9ca3dad61a22fa6a6bc32a411096b1142
Tom Radford <tom@theradford.com>
2024-04-30 08:07:19 +0200

    add new office route

---
+++ src/pages/office/new.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/common/Header";
+import EditOffice from "@/components/office/EditOffice";
+import { TITLE } from "@/utils/constants";
+import Head from "next/head";
+
+const AddOfficePage = () => {
+  return (
+    <>
+      <Head>
+        <title>New Office | {TITLE}</title>
+      </Head>
+      <Header title="New Office" />
+      <EditOffice />
+    </>
+  );
+};
+export default AddOfficePage;
