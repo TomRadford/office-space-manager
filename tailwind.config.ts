e9d62eafa685a13eca03404ef4429ce8aa87fc1e
Tom Radford <tom@theradford.com>
2024-04-30 09:35:47 +0200

    add shimmer keyframes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
-import colors, { red } from "tailwindcss/colors";
+import colors, { transparent } from "tailwindcss/colors";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
@@ -15,12 +15,20 @@ export default {
       dark: "#292D32",
       red: "#F44336",
       selected: "#475569",
+      transparent: transparent,
       // Office colours are defined in the `OFFICE_COLOURS` constant
     },
     // Keeping tailwind defaults (sans colours) for convenience
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      keyframes: {
+        shimmer: {
+          "100%": {
+            transform: "translateX(100%)",
+          },
+        },
       },
     },
   },
