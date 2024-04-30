d8da4b1b6a2b66c7df417684c11d8953a56a17a0
Tom Radford <tom@theradford.com>
2024-04-30 07:25:05 +0200

    forward ref on TextInput

--- src/components/common/input/TextInput.tsx
+++ src/components/common/input/TextInput.tsx
@@ -1,10 +1,20 @@
-const TextInput = () => {
+import { forwardRef, type InputHTMLAttributes } from "react";
+
+// We pass the ref to the input element,
+// this is necessary for react-hook-form
+// to do it's magic 🪄
+const TextInput = forwardRef<
+  HTMLInputElement,
+  InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => {
   return (
     <input
-      type="text"
-      className="w-full border-0 placeholder:opacity-50 focus:ring-secondary"
-      placeholder="Office Name"
+      className="invalid:ring-red-500 w-full border-0 placeholder:opacity-50 invalid:ring-1 focus:ring-secondary"
+      {...props}
+      ref={ref}
     />
   );
-};
+});
+
+TextInput.displayName = "TextInput";
 export default TextInput;
