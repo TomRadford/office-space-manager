1886be842e3eb05cb6b70cf9f4aea0b6ad85faec
Tom Radford <tom@theradford.com>
2024-04-30 08:05:52 +0200

    add error message to text input

--- src/components/common/input/TextInput.tsx
+++ src/components/common/input/TextInput.tsx
@@ -1,18 +1,39 @@
 import { forwardRef, type InputHTMLAttributes } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 // We pass the ref to the input element,
 // this is necessary for react-hook-form
 // to do it's magic 🪄
 const TextInput = forwardRef<
   HTMLInputElement,
-  InputHTMLAttributes<HTMLInputElement>
+  InputHTMLAttributes<HTMLInputElement> & { errorMessage?: string }
 >((props, ref) => {
   return (
-    <input
-      className="invalid:ring-red-500 w-full border-0 placeholder:opacity-50 invalid:ring-1 focus:ring-secondary"
-      {...props}
-      ref={ref}
-    />
+    <div className="relative">
+      <input
+        className="invalid:ring-red-500 w-full border-0 placeholder:opacity-50 invalid:ring-1 focus:ring-secondary"
+        {...props}
+        ref={ref}
+      />
+      <AnimatePresence>
+        {props.errorMessage && (
+          // TODO get correct validation feedback
+          // from the design team, this one is a placeholder
+          // so that the user can see the error message so long
+          <div className="absolute -bottom-4 left-3">
+            <motion.p
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ type: "tween" }}
+              className=" text-xs text-red"
+            >
+              {props.errorMessage}
+            </motion.p>
+          </div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 });
 
