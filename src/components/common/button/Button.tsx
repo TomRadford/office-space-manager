e3c0a6021f110a3c997d97633a662acb0c5eaed4
Tom Radford <tom@theradford.com>
2024-05-01 14:13:10 +0200

    update button ghost variant

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -4,7 +4,7 @@ import type { ButtonHTMLAttributes, ReactNode } from "react";
 const buttonStyles = {
   regular: "bg-secondary",
   red: "bg-red",
-  ghost: "bg-white text-secondary",
+  ghost: "bg-transparent text-secondary",
 };
 
 const Button = ({
@@ -28,7 +28,7 @@ const Button = ({
       // cn util is used here to resolve conflicting styles
       // Note: Button is always fixed width and height
       className={cn(
-        "mx-auto flex h-12 w-[232px] items-center justify-center rounded-[100px] p-4 text-white",
+        "mx-auto flex h-12 w-[232px] items-center justify-center rounded-[100px] p-4 text-sm font-medium uppercase text-white hover:opacity-90",
         { "opacity-80": disabled },
         buttonStyles[variant],
       )}
