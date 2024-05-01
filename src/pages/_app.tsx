bb3fe23b44b3ad71aba516ae8c485051e3090a4c
Tom Radford <tom@theradford.com>
2024-05-01 16:40:05 +0200

    add Query devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
 import NiceModal from "@ebay/nice-modal-react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { api } from "@/utils/api";
 
@@ -12,15 +13,18 @@ export const inter = Inter({
 });
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <NiceModal.Provider>
-      <main
-        className={`font-sans ${inter.variable} min-h-screen bg-background`}
-      >
-        <div className="mx-auto max-w-3xl px-4">
-          <Component {...pageProps} />
-        </div>
-      </main>
-    </NiceModal.Provider>
+    <>
+      <NiceModal.Provider>
+        <main
+          className={`font-sans ${inter.variable} min-h-screen bg-background`}
+        >
+          <div className="mx-auto max-w-3xl px-4">
+            <Component {...pageProps} />
+          </div>
+        </main>
+      </NiceModal.Provider>
+      <ReactQueryDevtools />
+    </>
   );
 };
 
