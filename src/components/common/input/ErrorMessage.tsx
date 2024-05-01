8f8fde75f30a4f5a392eaadb05c15442dac2678d
Tom Radford <tom@theradford.com>
2024-05-01 19:04:44 +0200

    use JSDoc for ErrorMessage

--- src/components/common/input/ErrorMessage.tsx
+++ src/components/common/input/ErrorMessage.tsx
@@ -1,9 +1,11 @@
 import { motion } from "framer-motion";
 
+/**
+ * @todo get correct validation feedback
+ * from the design team, this one is a placeholder
+ * so that the user can see the error message so long
+ */
 const ErrorMessage = ({ errorMessage }: { errorMessage: string }) => {
-  // TODO get correct validation feedback
-  // from the design team, this one is a placeholder
-  // so that the user can see the error message so long
   return (
     <motion.p
       initial={{ opacity: 0, y: -10 }}
