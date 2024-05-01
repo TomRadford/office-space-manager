f48cbdf3fbd121adf4ea9c65b9f2bbf19367116b
Tom Radford <tom@theradford.com>
2024-05-01 17:56:26 +0200

    add staffMember input schema

---
+++ src/inputSchema/staffMember.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+/**
+ * Schema for staff member input validation
+ */
+export const staffMemberInputSchema = z.object({
+  /** Not needed for new staff member */
+  id: z.number().optional(),
+  firstName: z.string().min(2),
+  lastName: z.string().min(2),
+  avatarUri: z.string(),
+  officeId: z.number(),
+});
