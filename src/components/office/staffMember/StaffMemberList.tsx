0c5443ee5c00a0dbb0d32efe535248af83c36e04
Tom Radford <tom@theradford.com>
2024-05-01 19:05:02 +0200

    render list for staff members

--- src/components/office/staffMember/StaffMemberList.tsx
+++ src/components/office/staffMember/StaffMemberList.tsx
@@ -1,8 +1,13 @@
 import SearchInput from "@/components/common/input/SearchInput";
-import { StaffMember } from "@prisma/client";
+import StaffMember from "@/components/office/staffMember/StaffMember";
+import type { StaffMember as StaffMemberType } from "@prisma/client";
 import { useState } from "react";
 
-const StaffMemberList = ({ staffMembers }: { staffMembers: StaffMember[] }) => {
+const StaffMemberList = ({
+  staffMembers,
+}: {
+  staffMembers: StaffMemberType[];
+}) => {
   const [search, setSearch] = useState("");
   // ToDo: Implement search functionality through API instead of filtering in the ui
   return (
@@ -11,6 +16,11 @@ const StaffMemberList = ({ staffMembers }: { staffMembers: StaffMember[] }) => {
       <div className="mt-6 flex items-center justify-between">
         <h3 className="font-semibold">Staff Members in Office</h3>
         <p className=" text-lg font-normal">{staffMembers.length}</p>
+      </div>
+      <div className="mt-6">
+        {staffMembers.map((staffMember) => (
+          <StaffMember staffMember={staffMember} key={staffMember.id} />
+        ))}
       </div>
     </div>
   );
