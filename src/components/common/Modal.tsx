4898d36e9460023452e9cc428bb15e3d3a48ae5f
Tom Radford <tom@theradford.com>
2024-05-01 20:04:35 +0200

    add outside click support to modal

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -10,24 +10,37 @@ import { useAppModal } from "@/utils/hooks/modal";
  * @see useAppModal documentation for more information on how to use this component.
  *
  */
-const Modal = NiceModal.create(({ content }: { content: ReactNode }) => {
-  const modal = useAppModal();
-  return (
-    <>
-      <Dialog.Root open={true}>
-        <Dialog.Portal container={document.body}>
-          <Dialog.Overlay className="font-Sans fixed inset-0 bg-overlay/50 data-[state=open]:animate-overlayShow" />
-          <Dialog.Content
-            onEscapeKeyDown={() => modal.remove()}
-            // We need to add the font here since this portals outside of main div in _app.tsx
-            className={`font-sans ${inter.variable} fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-2xl translate-x-[-50%] translate-y-[-50%] rounded-lg bg-background px-4 py-6 focus:outline-none  data-[state=open]:animate-contentShow`}
-          >
-            {content}
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
-    </>
-  );
-});
+const Modal = NiceModal.create(
+  ({
+    content,
+    closeOnOutsideClick,
+  }: {
+    content: ReactNode;
+    closeOnOutsideClick?: boolean;
+  }) => {
+    const modal = useAppModal();
+    return (
+      <>
+        <Dialog.Root open={true}>
+          <Dialog.Portal container={document.body}>
+            <Dialog.Overlay
+              onClick={() => {
+                if (closeOnOutsideClick) modal.remove();
+              }}
+              className="font-Sans fixed inset-0 bg-overlay/50 data-[state=open]:animate-overlayShow"
+            />
+            <Dialog.Content
+              onEscapeKeyDown={() => modal.remove()}
+              // We need to add the font here since this portals outside of main div in _app.tsx
+              className={`font-sans ${inter.variable} fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-2xl translate-x-[-50%] translate-y-[-50%] rounded-lg bg-background px-4 py-6 focus:outline-none  data-[state=open]:animate-contentShow`}
+            >
+              {content}
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </>
+    );
+  },
+);
 
 export default Modal;
